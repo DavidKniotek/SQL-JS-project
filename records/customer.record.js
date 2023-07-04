e9b16785e3d72f147a9e720d83051a7684168cc7
DavidKniotek <dawid.kniotek@gmail.com>
2023-07-04 17:28:07 +0200

    mock has been created

---
+++ records/customer.record.js
@@ -0,0 +1,16 @@
+class CustomerRecord {
+    static listAll() {
+        return [
+            {
+                id: 'abc',
+                fullName: 'John Doe',
+                product: 'car leasing'
+            },
+            {
+                id: 'def',
+                fullName: 'Kate Boe',
+                product: 'mortgage loan'
+            }
+        ]
+    }
+}
