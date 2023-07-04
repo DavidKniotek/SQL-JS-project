fd11b4b56a108dcfdb1caac85754ae6175e8b8b7
DavidKniotek <dawid.kniotek@gmail.com>
2023-07-04 21:43:10 +0200

    POST method has been added to products

--- records/product.record.js
+++ records/product.record.js
@@ -1,17 +1,37 @@
+const {pool} = require("../utils/db");
+const {ValidationError} = require("../utils/errors");
+const {v4: uuid} = require('uuid');
+
 class ProductRecord {
-    static listAll() {
-        return [
-            {
-                id: 'abc1',
-                productName: 'Car leasing',
-                periodInYears: 3
-            },
-            {
-                id: 'def1',
-                productName: 'Mortgage loan',
-                periodInYears: 20
-            },
-        ];
+    constructor(obj) { // Errors validation, which references to handleError middleware.
+        if (!obj.productName || obj.productName.length < 3 || obj.productName.length > 30) {
+            throw new ValidationError('Product name should contain at least 3 signs.');
+        }
+
+        if (!obj.periodInYears || obj.periodInYears < 1 || obj.periodInYears > 99) {
+            throw new ValidationError('Financing period should be at least 1 year, and do not exceed 99 years.');
+        }
+
+        this.id = obj.id;
+        this.productName = obj.productName;
+        this.periodInYears = obj.periodInYears;
+    }
+
+    async insert() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+
+        await pool.execute("INSERT INTO `products` VALUES(:id, :productName, :periodInYears)", {
+            id: this.id,
+            productName: this.productName,
+            periodInYears: this.periodInYears,
+        });
+        return this.id;
+    }
+    static async listAll() {
+        const [results] = await pool.execute("SELECT * FROM `products`");
+        return results;
     }
 }
 
--- routers/product-router.js
+++ routers/product-router.js
@@ -3,14 +3,28 @@ const {ProductRecord} = require("../records/product.record");
 const productsRouter = Router();
 
 productsRouter
-        .get('/', (req, res) => {
+        .get('/', async (req, res) => {
 
-            const productsList = ProductRecord.listAll();
+            const productsList = await ProductRecord.listAll();
 
             res.render('products/products-list', {
                 productsList,
             });
-        });
+        })
+
+        .post('/', async (req, res) => {
+            console.log(req.body)
+            const data = {
+                ...req.body,
+                periodInYears: Number(req.body.periodInYears),
+            };
+            console.log(data);
+
+            const newProduct = new ProductRecord(data);
+            await newProduct.insert();
+
+            res.redirect('/products');
+        })
 
 module.exports = {
     productsRouter,
