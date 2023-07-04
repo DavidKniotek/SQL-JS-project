f313df6d94a63f68f4dd37a4d61c6b474ffb0837
DavidKniotek <dawid.kniotek@gmail.com>
2023-07-04 18:26:32 +0200

    customers list upgrade

--- routers/customer-router.js
+++ routers/customer-router.js
@@ -1,14 +1,17 @@
 const {Router} = require('express');
 const {CustomerRecord} = require("../records/customer.record");
+const {ProductRecord} = require("../records/product.record");
 const customerRouter = Router();
 
 customerRouter
         .get('/', (req, res) => {
 
             const customersList = CustomerRecord.listAll();
+            const productsList = ProductRecord.listAll()
 
             res.render('customers/customers-list', {
                 customersList,
+                productsList,
             });
         });
 
