e3ca3772375e7790310846062652741fef0ee94d
DavidKniotek <dawid.kniotek@gmail.com>
2023-07-04 16:55:53 +0200

    update

--- index.js
+++ index.js
@@ -9,17 +9,24 @@ app.use(express.urlencoded({
     extended: true,
 }));
 app.use(express.static('public'));
+app.use(handleError); // In case of using an own middleware to errors handling.
+
 app.engine('.hbs', engine({
     extname: '.hbs',
     // helpers: handlebarsHelpers, // Additional modules, which could be useful during work with handlebars.
 }));
 app.set('view engine', '.hbs'); // Settings for layouts/views engine.
 
+
+
 app.get('/', (req, res) => {
-    res.send('Working');
+    res.redirect('/customers');
+});
+app.get('/customers', (req, res) => {
+    res.render('customers/list');
 });
 
-app.use(handleError); // In case of using an own middleware to errors handling.
+
 
 app.listen(3000, 'localhost', () => {
     console.log('Listening...')
